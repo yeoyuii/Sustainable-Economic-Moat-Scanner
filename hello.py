f9a9539b6a00dd9de41640d33672cec66214f509
yeoyuii <ioeuui@gmail.com>
2025-12-21 20:40:46 -0500

    Implement stock data collector function
    Added a function to collect stock data using yfinance.

--- hello.py
+++ hello.py
@@ -1,1 +1,24 @@
-print("hello world")
+import pandas as pd
+import yfinance as yf
+
+def data_collector(ticker):
+    '''
+    Retreiving stock information and returning selective data
+    '''
+    stock = yf.Ticker(ticker)
+    info = stock.info
+
+    # Storing information into a dictionary
+    data = {
+        'Stock' : info.get('longName', ticker),
+        'Price' : info.get('currentPrice', 0),
+        'Market_cap' : info.get('marketCap', 0)
+    }
+
+    return data 
+
+aapl_data = data_collector("AAPL")
+print(aapl_data)
+
+apple = yf.Ticker('AAPL')
+print(apple.info)
