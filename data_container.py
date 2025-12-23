3a29c27c14b35bfee95c2d680ffa0d47b1c5bbf5
yeoyuii <ioeuui@gmail.com>
2025-12-22 19:19:57 -0500

    Implement financial calculation functions
    Added functions for financial calculations including COGS, net revenue, gross profit, EBIT, and NOPAT.

--- data_container.py
+++ data_container.py
@@ -17,6 +17,56 @@ def data_collector(ticker):
 
     return data 
 
+def calc_COGS(ticker):
+    '''
+    Calculating cost of goods
+    formula: COGS = beginning inventory + purchases in current period - ending inventory
+    '''
+    beginning_inv = ticker.info.get('sharesOutstanding')
+    cost_of_goods = beginning_inv + curr_purchase_period - ending_inv
+
+def calculate_netRev():
+    '''
+    Docstring for calculate_rev
+    Net Revenue = Gross Revenue - Returns - Discounts - Sales Allowances
+    '''
+
+def forecast_rev():
+    '''
+    Docstring for forecast_rev
+    revenue = price x quantity
+    '''
+
+def calculate_gross_profit():
+    '''
+    Docstring for gross proft
+    Gross Profit = Net Revenue - Cost of Goods Sold (COGS)
+    '''
+
+def calculate_EBIT():
+    '''
+    Docstring for EBIT
+    EBIT stands for “Earnings Before Interest and Taxes”
+    EBIT = gross profit - operating expenses
+    '''
+
+
+def calculate_NOPAT():
+    '''
+    Docstring for calculate_NOPAT
+    NOPAT stands for Net Operating Profit After Tax
+    NOPAT = EBIT x (1 - Tax rate)
+    '''
+
+
+
+
+
+
+
+
+
+
 aapl_data = data_collector("AAPL")
 print(aapl_data)
 
