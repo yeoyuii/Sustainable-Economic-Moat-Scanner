503872da56ab225f3d18faeab9188aec8e65e7a2
yeoyuii <ioeuui@gmail.com>
2025-12-23 13:45:48 -0500

    Refactor data_collector to a class structure

--- data_container.py
+++ data_container.py
@@ -1,69 +1,38 @@
 import pandas as pd
 import yfinance as yf
 
-def data_collector(ticker):
-    '''
-    Retreiving stock information and returning selective data
-    '''
-    stock = yf.Ticker(ticker)
-    info = stock.info
+class data_collector():
+    def __init__(self, ticker):
+        self.ticker = ticker
 
-    # Storing information into a dictionary
-    data = {
-        'Stock' : info.get('longName', ticker),
-        'Price' : info.get('currentPrice', 0),
-        'Market_cap' : info.get('marketCap', 0)
-    }
+    def data_collector(ticker):
+        '''
+        Retreiving stock information and returning selective data
+        '''
+        stock = yf.Ticker(ticker)
+        info = stock.info
 
-    return data 
+        # Storing information into a dictionary
+        data = {
+            'Stock' : info.get('longName', ticker),
+            'Price' : info.get('currentPrice', 0),
+            'Market_cap' : info.get('marketCap', 0)
+        }
 
-def calc_COGS(ticker):
-    '''
-    Calculating cost of goods
-    formula: COGS = beginning inventory + purchases in current period - ending inventory
-    '''
-    beginning_inv = ticker.info.get('sharesOutstanding')
-    cost_of_goods = beginning_inv + curr_purchase_period - ending_inv
+        return data 
+    
+    def calculate_TaxRate():
+        '''
+        Docstring for calculate_TaxRate
+        Tax Rate = Tax provision/pretax income
+        '''
 
-def calculate_netRev():
-    '''
-    Docstring for calculate_rev
-    Net Revenue = Gross Revenue - Returns - Discounts - Sales Allowances
-    '''
-
-def forecast_rev():
-    '''
-    Docstring for forecast_rev
-    revenue = price x quantity
-    '''
-
-def calculate_gross_profit():
-    '''
-    Docstring for gross proft
-    Gross Profit = Net Revenue - Cost of Goods Sold (COGS)
-    '''
-
-def calculate_EBIT():
-    '''
-    Docstring for EBIT
-    EBIT stands for “Earnings Before Interest and Taxes”
-    EBIT = gross profit - operating expenses
-    '''
-
-
-def calculate_NOPAT():
-    '''
-    Docstring for calculate_NOPAT
-    NOPAT stands for Net Operating Profit After Tax
-    NOPAT = EBIT x (1 - Tax rate)
-    '''
-
-
-
-
-
-
-
+    def calculate_NOPAT():
+        '''
+        Docstring for calculate_NOPAT
+        NOPAT stands for Net Operating Profit After Tax
+        NOPAT = EBIT x (1 - Tax rate)
+        '''
 
 
 
