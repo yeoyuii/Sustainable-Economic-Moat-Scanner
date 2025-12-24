9c12cd5030dadb1f40e32a46f63f600deb36ae31
yeoyuii <ioeuui@gmail.com>
2025-12-24 18:02:51 -0500

    Refactor data_collector methods to use instance variables
    Updated methods in data_collector class to use 'self' and corrected ticker usage.

--- data_container.py
+++ data_container.py
@@ -5,11 +5,11 @@ class data_collector():
     def __init__(self, ticker):
         self.ticker = ticker
 
-    def data_collector(ticker):
+    def data_collector(self, ticker):
         '''
         Retreiving stock information and returning selective data
         '''
-        stock = yf.Ticker(ticker)
+        stock = yf.Ticker(self.ticker)
         info = stock.info
 
         # Storing information into a dictionary
@@ -21,19 +21,31 @@ class data_collector():
 
         return data 
     
-    def calculate_TaxRate():
+    def calculate_TaxRate(self):
         '''
         Docstring for calculate_TaxRate
         Tax Rate = Tax provision/pretax income
         '''
+        income_stmt = self.ticker.get_income_stmt() 
 
-    def calculate_NOPAT():
+        operating_income = income_stmt.loc['OperatingIncome']
+
+        # iloc[0] = Most recent period (TTM for annual, latest quarter for quarterly)
+        most_recent_op_income = operating_income.iloc[0]
+        tax_expense = income_stmt.loc['TaxProvision'].iloc[0]
+        pretax_income = income_stmt.loc['PretaxIncome'].iloc[0]
+
+        effective_tax_rate = tax_expense / pretax_income
+
+        return effective_tax_rate
+
+
+    def calculate_NOPAT(self):
         '''
         Docstring for calculate_NOPAT
         NOPAT stands for Net Operating Profit After Tax
         NOPAT = EBIT x (1 - Tax rate)
         '''
-
 
 
 aapl_data = data_collector("AAPL")
