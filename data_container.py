8b4f27e27122ce1b2d3b8ae38f39d2145fc54cc8
yeoyuii <ioeuui@gmail.com>
2025-12-25 22:28:00 -0500

    Refactor data_collector to store income statement

--- data_container.py
+++ data_container.py
@@ -4,6 +4,8 @@ import yfinance as yf
 class data_collector():
     def __init__(self, ticker):
         self.ticker = ticker
+        self.income_stmt = self.ticker.get_income_stmt()
+        self.effective_tax_rate = 0
 
     def data_collector(self, ticker):
         '''
@@ -26,18 +28,17 @@ class data_collector():
         Docstring for calculate_TaxRate
         Tax Rate = Tax provision/pretax income
         '''
-        income_stmt = self.ticker.get_income_stmt() 
 
-        operating_income = income_stmt.loc['OperatingIncome']
+        operating_income = self.income_stmt.loc['OperatingIncome']
 
         # iloc[0] = Most recent period (TTM for annual, latest quarter for quarterly)
         most_recent_op_income = operating_income.iloc[0]
-        tax_expense = income_stmt.loc['TaxProvision'].iloc[0]
-        pretax_income = income_stmt.loc['PretaxIncome'].iloc[0]
+        tax_expense = self.income_stmt.loc['TaxProvision'].iloc[0]
+        pretax_income = self.income_stmt.loc['PretaxIncome'].iloc[0]
 
-        effective_tax_rate = tax_expense / pretax_income
+        self.effective_tax_rate = tax_expense / pretax_income
 
-        return effective_tax_rate
+        return self.effective_tax_rate
 
 
     def calculate_NOPAT(self):
@@ -46,6 +47,11 @@ class data_collector():
         NOPAT stands for Net Operating Profit After Tax
         NOPAT = EBIT x (1 - Tax rate)
         '''
+        getting_EBIT = self.income_stmt.loc['EBIT']
+        calc_EBIT = self.income_stmt.loc['EBIT'].iloc[0]
+        calc_NOPAT = calc_EBIT * (1 - self.effective_tax_rate)
+        return calc_NOPAT
+
 
 
 aapl_data = data_collector("AAPL")
